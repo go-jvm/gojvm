03bceb18c4f1024d02155db1064ee42fa150b743
a289237642 <a289237642@163.com>
2018-11-25 20:58:26 +0800

    demo

---
+++ demo01/main.go
@@ -0,0 +1,28 @@
+package main
+import "fmt"
+//定义全局变量
+// var n1=100
+// var n2=200
+// var name="tom"
+//改成一次性声明
+var (
+	 n3=100
+	 n4=200
+	 name1="jack"
+)
+func main(){
+	// fmt.Println("n1=",n1,"n2=",n2,"name=",name)
+	fmt.Println("n3=",n3,"n4=",n4,"name1=",name1)
+	// var i int
+	// i=10
+	// fmt.Println("i=",i)
+	// name:="lg"
+	// fmt.Println("name=",name)
+	// var n1,n2,n3 int
+	// fmt.Println("n1=",n1,"n2=",n2,"n3=",n3)
+	// var n1,name,n3=100,"lg",888
+	// fmt.Println("n1=",n1,"name=",name,"n3=",n3)
+	// n1,name,n3:=100,"lg",888
+	// fmt.Println("n1=",n1,"name=",name,"n3=",n3)
+
+}
