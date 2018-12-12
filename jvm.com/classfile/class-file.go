c5bbfe1deceaf1bf0ed139b34ab92fe565930080
kekeon <liuke095@gmail.com>
2018-12-13 00:05:44 +0800

    update paser function

--- jvm.com/classfile/class-file.go
+++ jvm.com/classfile/class-file.go
@@ -3,21 +3,21 @@ package classfile
 import "fmt"
 
 type ClassFile struct {
-	minorVersion	uint16
-	majorVersion	uint16
-	constantPool	ConstantPool
-	accessFlags		uint16
-	thisClass		uint16
-	superClass 		uint16
-	interfaces 		[]uint16
-	fields 			[]*MemberInfo
-	methods 		[]*MemberInfo
-	attributes 		[]AttributeInfo
+	minorVersion uint16
+	majorVersion uint16
+	constantPool ConstantPool
+	accessFlags  uint16
+	thisClass    uint16
+	superClass   uint16
+	interfaces   []uint16
+	fields       []*MemberInfo
+	methods      []*MemberInfo
+	attributes   []AttributeInfo
 }
 
 func Parse(classData []byte) (cf *ClassFile, err error) {
 	defer func() {
-		if r:= recover(); r != nil {
+		if r := recover(); r != nil {
 			var ok bool
 			err, ok = r.(error)
 
@@ -60,7 +60,6 @@ func (self *ClassFile) MajorVersion() uint16 {
 	return self.majorVersion
 }
 
-
 func (self *ClassFile) ClassName() string {
 	return self.constantPool.getClassName(self.thisClass)
 }
@@ -75,12 +74,37 @@ func (self *ClassFile) SuperClassName() string {
 }
 
 func (self *ClassFile) InterfaceNames() []string {
-	interfaceNames := make([]string,len(self.interfaces))
+	interfaceNames := make([]string, len(self.interfaces))
 
-	for i,cpIndex := range interfaceNames {
+	for i, cpIndex := range interfaceNames {
 		interfaceNames[i] = self.constantPool.getClassName(cpIndex)
 	}
 
 	return interfaceNames
 }
 
+func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
+	magic := reader.readUint32()
+
+	if magic != 0xCAFEBABE {
+		panic("java.lang.ClassFormatError:magic!")
+	}
+}
+
+func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
+	self.minorVersion = reader.readUint16()
+
+	self.majorVersion = reader.readUint16()
+
+	switch self.majorVersion {
+	case 45:
+		return
+	case 46, 47, 48, 49, 50, 51, 52:
+
+		if self.minorVersion == 0 {
+			return
+		}
+	}
+
+	panic("java.lang.UnsupportedClassVersionError!")
+}
