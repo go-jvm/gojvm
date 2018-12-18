fe5869a21c5d59ad662721c8d19624512aaa6167
kekeon <liuke095@gmail.com>
2018-12-19 00:12:16 +0800

    add constant pool base type

--- jvm.com/classfile/cp-numeric.go
+++ jvm.com/classfile/cp-numeric.go
@@ -1,5 +1,41 @@
 package classfile
 
+import "math"
+
 type ConstantIntegerInfo  struct {
 	val int32
+}
+
+type ConstantFloatInfo struct {
+	val float32
+}
+
+type ConstantLongInfo struct {
+	val int64
+}
+
+type ConstantDoubleInfo struct {
+	val float64
+}
+
+func (self *ConstantIntegerInfo) readInfo(reader *ClassReader) {
+	bytes := reader.readUint32()
+	self.val = int32(bytes)
+}
+
+func (self *ConstantFloatInfo) readInfo(reader *ClassReader) {
+	bytes := reader.readUint32()
+	self.val = math.Float32frombits(bytes)
+}
+
+
+func (self *ConstantLongInfo) readInfo(reader *ClassReader) {
+	bytes := reader.readUint64()
+
+	self.val = int64(bytes)
+}
+
+func (self *ConstantDoubleInfo) readInfo(reader *ClassReader) {
+	bytes := reader.readUint64()
+	self.val = math.Float64frombits(bytes)
 }
